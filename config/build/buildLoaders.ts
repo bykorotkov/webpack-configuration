2205e217e1bfaf9b33defca9ea1530b883210326
Igor Korotkov <bykorotkov@gmail.com>
2023-12-17 01:16:01 +0400

    aliases, assets, global env, type checker, hmr, favicon, babel-loader, source-map added and configured

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,8 @@
 import {ModuleOptions} from 'webpack'
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BuildOptions} from "./types/types";
+import ReactRefreshTypeScript from 'react-refresh-typescript'
+import {buildBabelLoader} from "./babel/buildBabelLoader";
 
 export function buildLoaders(options: BuildOptions):ModuleOptions['rules'] {
     const isDev = options.mode === 'development';
@@ -12,6 +14,34 @@ export function buildLoaders(options: BuildOptions):ModuleOptions['rules'] {
                     localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]'
                 }
             },
+    }
+
+    const assetLoader =  {
+            test: /\.(png|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+    }
+
+    const svgrLoader =  {
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: [
+                {
+                    loader: '@svgr/webpack',
+                    options: {
+                        icon: true,
+                        svgoConfig: {
+                            plugins: [
+                                {
+                                    name: 'convertColors',
+                                    params: {
+                                        currentColor: true
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                }
+            ],
     }
 
     const scssLoader = {
@@ -28,12 +58,27 @@ export function buildLoaders(options: BuildOptions):ModuleOptions['rules'] {
 
     const tsLoader = {
         test: /\.tsx?$/,
-        use: 'ts-loader',
         exclude: /node_modules/,
+        use: [
+            {
+                loader: 'ts-loader',
+                options: {
+                    transpileOnly: true,
+                    getCustomTransformers: () => ({
+                        before: [isDev && ReactRefreshTypeScript()].filter(Boolean),
+                    }),
+                }
+            }
+        ]
     }
 
+    const babelLoader = buildBabelLoader(options)
+
     return [
+        svgrLoader,
+        assetLoader,
         scssLoader,
-        tsLoader
+        // tsLoader
+        babelLoader
     ]
 }
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,55 @@
 import React, {useState} from 'react';
 import classes from './App.module.scss'
 import {Link, Outlet} from "react-router-dom";
+import About from "@/pages/about/About";
+import imagePng from '@/assets/bannerCareerTablet1.png'
+import imageJpg from '@/assets/TourBgImage.jpg'
+import ImageSvg from '@/assets/closeIcon.svg'
+
+function TODO() {
+    TODO2()
+}
+
+function TODO2() {
+    throw new Error();
+}
 
 export const App = () => {
     const [count, setCount] = useState<number>(0)
 
-    const increment = () => setCount(prev => prev + 1 )
+    const increment = () => {
+        // setCount(prev => prev + 1 )
+        TODO()
+    }
+    // TODO(213123)
+    // if(__PLATFORM__ === 'desktop') {
+    //     return <div>ISDESKTOPPLATFORM</div>
+    // }
+    //
+    // if(__PLATFORM__ === 'mobile') {
+    //     return <div>ISMOBILEPLATFORM</div>
+    // }
+    //
+    // if(__ENV__ === 'development') {
+    //     // addDevtools()
+    // }
 
     return (
-        <div>
+        <div data-testid={'App.DataTestId'}>
+            <h1 data-testid={'Platform'}>PLATFORM={__PLATFORM__}</h1>
+            <div>
+                <img width={300} height={300} src={imagePng} alt={''} />
+                <img width={300} height={300} src={imageJpg} alt={''} />
+            </div>
+            <div>
+                <ImageSvg color={'blue'}  width={50} height={50} />
+            </div>
             <Link to={'/about'}>about</Link>
             <br />
             <Link to={'/shop'}>shop</Link>
             <h1 className={classes.value}>{count}</h1>
             <button className={classes.button} onClick={increment}>increment</button>
-            <Outlet />
+            <About />
         </div>
     );
 };
